d6670544ce6b738156683b5388af9d5883304c5e
Rafael <ribeirodelimarafael@gmail.com>
2024-11-17 12:43:44 -0300

    add mongodb and mongoose dependencies; creates gitignore file

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.send('Nodemon test')
   })
   
 app.listen(3000)
 
-console.log('Server is running on port 3000')
+console.log('Server is running on port http://localhost:3000')
+
+mongoose.connect('mongodb+srv://admin:<db_password>@backenddb.hldza.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB')
