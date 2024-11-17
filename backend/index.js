70c422287418402ecdfc1eaab22d807f5f0bc701
Rafael <ribeirodelimarafael@gmail.com>
2024-11-17 12:54:02 -0300

    add mongoose connection to the database

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,27 @@
 const express = require('express')
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
 const app = express()
+
+const USER = process.env.USER
+const PASSWORD = process.env.PASSWORD
+const PORT = process.env.PORT
+const project_name = 'Node-API'
+
 
 app.get('/', function (req, res) {
     res.send('Nodemon test')
   })
   
-app.listen(3000)
+app.listen(PORT)
 
-console.log('Server is running on port http://localhost:3000')
+console.log(`Server is running on port http://localhost:${PORT}`)
 
-mongoose.connect('mongodb+srv://admin:<db_password>@backenddb.hldza.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB')
+mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@backenddb.hldza.mongodb.net/${project_name}?retryWrites=true&w=majority&appName=BackendDB`)
+.then(() => {
+  console.log('connected to the database')
+})
+.catch(() => {
+  console.log('connection failed')
+})
