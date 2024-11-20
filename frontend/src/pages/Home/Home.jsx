6478736d384bc869d9e48dbac4e03d4b208ac506
Maria Betman <maria.betman03@gmail.com>
2024-11-20 17:26:06 -0300

    Style base setup

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import './Home.scss';
+import Logo from '../../assets/images/logo.png'
 
 export default function Home() {
 
   return (
     <div id="home">
       <h2>Home</h2>
+      <img src={Logo} alt=''/>
     </div>
   )
 }
