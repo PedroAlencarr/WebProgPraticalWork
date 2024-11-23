7d588cb916d13041bd19222c33fd7c1a7af2a29f
Rafael <ribeirodelimarafael@gmail.com>
2024-11-23 18:05:29 -0300

    add user model and get/post methods for user in index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
+const User = require('./models/user.model.js')
 const app = express()
+
+app.use(express.json())
 
 const USER = process.env.USER
 const PASSWORD = process.env.PASSWORD
@@ -12,15 +15,59 @@ const project_name = 'Node-API'
 
 app.get('/', function (req, res) {
     res.send('Nodemon test')
-  })
-  
-app.listen(PORT)
+})
 
-console.log(`Server is running on port http://localhost:${PORT}`)
+app.post('/api/users', async (req, res) => {
+  try {
+    const user = await User.create(req.body)
+    res.status(200).send(user)
+  } catch (error) {
+    res.status(500).json({error: error.message})
+  }
+})  
+
+app.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find({})
+    res.status(200).send(users)
+  } catch (error) {
+    res.status(500).json({error: error.message})
+  }
+})
+
+app.get('/api/user/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const user = await User.findById(id)
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).json({error: error.message})
+  }
+})
+
+app.put('/api/user/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const user = await User.findByIdAndUpdate(id, req.body)
+    
+    if (!user) {
+      res.status(404).json({message: 'User not found'})
+    }
+
+    const updatedUser = await User.findById(id)   
+    res.status(200).json(updatedUser)
+
+  } catch (error) {
+    res.status(500).json({error: error.message})
+  }
+})
 
 mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@backenddb.hldza.mongodb.net/${project_name}?retryWrites=true&w=majority&appName=BackendDB`)
 .then(() => {
   console.log('connected to the database')
+  app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`)
+  })
 })
 .catch(() => {
   console.log('connection failed')
