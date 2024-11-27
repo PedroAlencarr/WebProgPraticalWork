8e2e4b50d2d2fab0cfce23b7b7e44cce0dc39545
Maria Betman <maria.betman03@gmail.com>
2024-11-26 21:37:54 -0300

    feat/Login page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import './assets/styles/index.scss';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Splash from './pages/Splash/Splash.jsx';
-import Register from './pages/Register/Register.jsx';
-import Login from './pages/Login/Login.jsx';
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
+import Splash from './pages/Splash/Splash.jsx';
+import Login from './pages/Login/Login.jsx';
+import Register from './pages/Register/Register.jsx';
 import Profile from './pages/Profile/Profile.jsx';
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Splash/>}/>
+            <Route path="/login" element={<Login/>}/>
             <Route path="/register" element={<Register/>}/>
             <Route path="/profile" element={<Profile/>}/>
           </Routes>
